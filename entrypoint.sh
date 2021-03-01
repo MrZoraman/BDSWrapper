e4f1a40a884109ef88d1de467e209ccf4583eed8
MrZoraman <MrZoraman@gmail.com>
2021-02-28 22:08:44 -0800

    it works!

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,8 @@
 set -m
 
 trap '{ echo "I have been hammered!"; }' SIGKILL
-trap '{ echo "I am not stopping"; echo "stop" > input; sleep 5; }' SIGTERM
 
 ./app.sh &
 pid=$!
-echo $pid
+trap '{ echo "I am not stopping"; echo "stop" > input; wait $pid; }' SIGTERM
 wait $pid
