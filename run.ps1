db015619aadcbd686cd74c73d6b77a4deb58524c
Sessa, Matt <matt.sessa@stryker.com>
2021-03-05 02:16:56 -0800

    Update run script

--- run.ps1
+++ run.ps1
@@ -1,1 +1,12 @@
-docker run -it --rm --name bds bds:latest
+New-Item -Path 'world' -ItemType Directory -Force
+
+$world = Join-Path -Path (Convert-Path .) -ChildPath "world"
+
+docker run `
+    --interactive `
+    --tty `
+    --rm `
+    --name bds `
+    --mount "type=bind,source=$world,target=/opt/bds/worlds" `
+    -p 127.0.0.1:19132:19132/udp `
+    bds:latest
